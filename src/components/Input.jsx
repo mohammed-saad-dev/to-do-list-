7b678a793dc3e3716eacc12df2fd2cd77f06941f
osd drake <83500947+mohammed-saad-dev@users.noreply.github.com>
2024-03-13 11:57:12 +0300

    done or not

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Input = () => {
     const [val, setVal] = useState(""); // input value 
     const [item, setItem] = useState([ "osd drake"]); // items i want to do 
-
+    const element = document.getElementsByTagName("li");
     function Delet(index){
         const newItem = [...item];
         newItem.splice(index, 1);
@@ -19,12 +19,27 @@ const Input = () => {
         }
     }
 
+    function get(index){
+        // console.log(item[index])
+        
+        element[index].className = element[index].className === "notDone" ? "done" : "notDone";
+    }
+    
     return (
         <div>
             <div>
-                <input onKeyPress={handleKeyPress} onChange={(e) => setVal(e.target.value)} value={val} placeholder='what about today ?' type="text" className='input'/>
-                <button onClick={() => val.trim().length === 0 ? null : (setItem([...item, val],setVal("")))} className='add'>Add</button>
-                <button style={{marginLeft:"10px"}} className='add' onClick={() => setItem([])}>clear</button>
+                <input
+                onKeyPress={handleKeyPress} 
+                onChange={(e) => setVal(e.target.value)} 
+                value={val} placeholder='what about today ?' type="text" className='input'/>
+
+                <button 
+                onClick={() => val.trim().length === 0 ? null : (setItem([...item, val],setVal("")))} 
+                className='add'>Add</button>
+                <button 
+                style={{marginLeft:"10px"}} 
+                className='add' onClick={() => setItem([])}>clear
+                </button>
             </div>
 
             <div>
@@ -32,7 +47,10 @@ const Input = () => {
                     {item.map((i,index) => {
                         return(
                                 <div key={index} className='wrab-items'>
-                                    <li key={index}><p>{i}</p> <button onClick={() =>Delet(index)} >Delete</button></li> 
+                                    <li  className="notDone" key={index}>
+                                        <p>{i}</p>
+                                        <button onClick={() =>Delet(index)} >Delete</button>
+                                        <button  onClick={() => get(index)}  >Done</button></li> 
                                 </div>
                                 )
                     })}
